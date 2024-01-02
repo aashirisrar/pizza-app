08b0aa2c89db0d81199d2ac6bc6e2f1f0265edf0
Aashir Israr <aashirisrar968@gmail.com>
2024-01-03 04:28:02 +0500

    Update button and link colors in hero and navbar components

--- components/hero.tsx
+++ components/hero.tsx
@@ -50,7 +50,7 @@ const Hero = () => {
         </span>
         <div className="flex items-center space-x-4 md:pb-10">
           <a
-            className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-[#D72E3B] text-primary shadow hover:bg-primary/90 h-9 px-4 py-2 rounded-[6px]"
+            className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-[#D72E3B] text-white shadow hover:bg-primary/90 h-9 px-4 py-2 rounded-[6px]"
             href="/docs"
           >
             Order Now
