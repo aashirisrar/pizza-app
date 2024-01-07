90d0b120837474e08ade153ccf4a0d37a32887ef
Aashir Israr <aashirisrar968@gmail.com>
2024-01-08 04:21:49 +0500

    Remove unused code and add dependencies

--- app/api/webhooks/route.ts
+++
@@ -1,6 +0,0 @@
-import { getUsers } from "@/lib/users";
-
-export async function POST(req: Request) {
-  const users = await getUsers();
-  console.log(users);
-}
--- lib/users.ts
+++
@@ -1,7 +0,0 @@
-import prismadb from "./prismadb";
-
-export const getUsers = async () => {
-  prismadb.user.create({
-    data: { name: "John", email: "aashirisrar@123@gmail.com" },
-  });
-};
