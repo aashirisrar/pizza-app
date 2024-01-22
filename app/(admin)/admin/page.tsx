bb725c6f76a6c98652ec9fd944b431ec8eb62a6f
Aashir Israr <aashirisrar968@gmail.com>
2024-01-22 06:13:29 +0500

    Add product details to AdminPage

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -5,11 +5,13 @@ import React, { useEffect, useState } from "react";
 
 const AdminPage = () => {
   const [orders, setOrders] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchOrders = async () => {
       const ord = await axios.post("api/getorders");
       console.log(ord.data.products);
+      setProducts(ord.data.products);
       setOrders(ord.data.data);
     };
 
@@ -22,10 +24,23 @@ const AdminPage = () => {
       <div>Orders:</div>
       <div>
         {orders.map((order: any) => (
-          <div key={order.id}>
+          <div className="mt-6" key={order.id}>
             <div>Order Id: {order.id}</div>
             <div>
               Order Status: {order.orderStatus ? "Delivered" : "Not Delivered"}
+            </div>
+            <div className="mt-2">Items:</div>
+            <div>
+              {products.map((product: any) => {
+                if (product.orderId === order.id) {
+                  return (
+                    <div className="flex" key={product.orderId}>
+                      <div>{product.name}</div>
+                      <div className="ml-2">x{product.quantity}</div>
+                    </div>
+                  );
+                }
+              })}
             </div>
             <div>Total Price: {order.totalPrice}</div>
           </div>
