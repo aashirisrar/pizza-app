36193d3e1efa29f01646cedcac7afc137c974d86
Aashir Israr <aashirisrar968@gmail.com>
2024-01-23 05:26:48 +0500

    Refactor admin page layout and styling

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -19,33 +19,46 @@ const AdminPage = () => {
   }, []);
 
   return (
-    <div>
-      <div>Admin Page</div>
-      <div>Orders:</div>
-      <div>
-        {orders.map((order: any) => (
-          <div className="mt-6" key={order.id}>
-            <div>Order Id: {order.id}</div>
-            <div>
-              Order Status: {order.orderStatus ? "Delivered" : "Not Delivered"}
-            </div>
-            <div className="mt-2">Items:</div>
-            <div>
-              {products.map((product: any) => {
-                if (product.orderId === order.id) {
-                  return (
-                    <div className="flex" key={product.orderId}>
-                      <div>{product.name}</div>
-                      <div className="ml-2">x{product.quantity}</div>
-                    </div>
-                  );
-                }
-              })}
-            </div>
-            <div>Total Price: {order.totalPrice}</div>
+    <div className="relative flex min-h-screen flex-col bg-background">
+      <main className="flex-1">
+        <div className="container1 relative">
+          <div className="text-center my-4 mb-10">
+            <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1] hidden md:block">
+              Orders
+            </h1>
           </div>
-        ))}
-      </div>
+          <div className="flex flex-col items-center">
+            {orders.map((order: any) => (
+              <div className="mb-6 shadow-lg rounded-xl p-4" key={order.id}>
+                <div>Order Id: {order.id}</div>
+                <div className="mt-2 text-[#D72E3B] font-semibold">
+                  Order Status:
+                  {order.orderStatus ? " Delivered" : " Not Delivered"}
+                </div>
+                <div className="mt-2 font-semibold">Items:</div>
+                <div>
+                  {products.map((product: any) => {
+                    if (product.orderId === order.id) {
+                      return (
+                        <div
+                          className="flex justify-between"
+                          key={product.orderId}
+                        >
+                          <div>{product.name}</div>
+                          <div>x{product.quantity}</div>
+                        </div>
+                      );
+                    }
+                  })}
+                </div>
+                <div className="mt-2 font-semibold text-lg">
+                  Total Price: {order.totalPrice}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
